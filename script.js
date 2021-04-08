562ef95ea20d589b3a29861a1d08c83f51453535
--globalDoniyor <--global>
2021-04-08 21:12:47 +0500

    versiya 1

---
+++ script.js
@@ -0,0 +1,29 @@
+// var a = 10
+
+// var b = 10
+
+// a+b 
+
+// // null
+// console.log(null)
+// // undefined
+// console.log(undefined)
+
+
+// // var , bn
+// var d =100, c=200
+// console.log('c+d=',d+ c)
+
+// console.log(a)
+// console.log(b)
+// console.log(a+b)
+// console.log(a*b)
+
+alert('Salom! Mening Ismim Doniyor')
+var javob=+prompt('Ismingizni yozing')
+var javob = +prompt('Yoshingizni yozing ')
+console.log(javob + 1)
+var javob = +prompt('Misollarni ishlang 5+3=')
+var javob1=prompt('Misolni ishlang  20*2')
+var javob2=prompt('Misolni ishlang 14+20')
+alert('Natijani korish uchun konsolga kiring')
